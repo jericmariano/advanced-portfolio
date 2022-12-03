e65033ff4613c370c9e0d8bc8be1f1d51fd1938c
Jeric <marianovjeric@gmail.com>
2022-12-02 22:45:00 -0800

    adjust contrast button complete

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// 
-// 
-// 
+let isModalOpen = false;
+let contrastToggle = false
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -25,7 +34,6 @@ function contact(event) {
     })
 }
 
-let isModalOpen = false;
 
 function toggleModal() {
     if (isModalOpen) {
